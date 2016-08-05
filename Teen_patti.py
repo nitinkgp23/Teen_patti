b0e5852da9aec7c134f5bcefb81c3781beccb73b
nitinkgp23 <nitin.iitkgp23@gmail.com>
2016-08-06 02:15:02 +0530

    Commenting

--- Teen_patti.py
+++ Teen_patti.py
@@ -1,6 +1,18 @@
 num = int(input("Enter number of players "))
 dict={}
+
+'''Inputting initial details about players  '''
+ 
 for i in range(1,num+1):
-	dict.update({i:input("Enter name of player "+str(i))})
+	dict.update({i:input("Enter name of player "+str(i)+' ')})
+
+#echo
 for x in dict.items():
 	print (x)
+
+
+INITIAL = int(input("Enter the initial amount"))
+balance={}
+for i in range(1,num+1):
+	balance.update({i:INITIAL})
+
