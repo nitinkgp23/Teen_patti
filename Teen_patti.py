6086f9b48ca2d6357837d0399b8e9bb44bf9c09c
nitinkgp23 <nitin.iitkgp23@gmail.com>
2016-08-07 16:32:37 +0530

    Issues resolved

--- Teen_patti.py
+++ Teen_patti.py
@@ -1,8 +1,9 @@
 def chance(temp_dict,stake):
 	temp=temp_dict
+	packed=[]
 	keys = temp.keys()
 	for x in keys:
-		prompt = input(temp[x])
+		prompt = input(temp[x]+' ')
 		if prompt.isdigit():
 			bet=int(prompt)
 		else:
@@ -13,18 +14,24 @@ def chance(temp_dict,stake):
 		stake+=bet
 		balance.update({x:(balance[x]-bet)})
 
-	#	if (bet==0):
-			#removing the player from current round
+		if (bet==0):
+			#a list of peope who have packed
 			#the player has packed
-			#del temp_dict[x]
+		        packed.append(x)
+	for x in packed:
+		del temp_dict[x]
 	return temp_dict,stake
 
 def round():
 	stake=0
-	temp_dict = data
+	temp_dict =data.copy() 
+	print('New Round!')
 	while(len(temp_dict)>1):
 		print('Input the betting money after each displayed name\n ')
 		temp_dict, stake =chance(temp_dict,stake)
+		# print(temp_dict)
+		# print(data)
+
 	return stake
 		
 stake=0
@@ -38,8 +45,7 @@ for i in range(1,num+1):
 	data.update({i:input("Enter name of player "+str(i)+' ')})
 
 #echo
-for x in data.items():
-	print (x)
+print(data)
 
 
 INITIAL = int(input("Enter the initial amount "))
@@ -55,4 +61,9 @@ while(gameStatus==1):
 	print('The current balance of each player is ')
 	for x in balance.keys():
 		print(str(x) + '	'+data[x]+'	'+str(balance[x]))
+		# print (str(x) + '	'+ str(balance[x])
 
+	'''print('balance')
+	print(balance)
+	print('data')
+	print(data)'''
