404db3fd8faa0eb6a281c33b430ae7a0ae157406
nitinkgp23 <nitin.iitkgp23@gmail.com>
2016-08-26 23:54:58 +0530

    Changed dict to Ordered Dict, added option for starting player

--- Teen_patti.py
+++ Teen_patti.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 def chance(temp_dict,stake):
 	temp=temp_dict
 	packed=[]
@@ -17,15 +18,24 @@ def chance(temp_dict,stake):
 		if (bet==0):
 			#a list of peope who have packed
 			#the player has packed
-		        packed.append(x)
+				packed.append(x)
 	for x in packed:
 		del temp_dict[x]
 	return temp_dict,stake
 
 def round():
-	stake=0
-	temp_dict =data.copy() 
+	stake=0 
 	print('New Round!')
+	temp_dict=OrderedDict()
+	start=int(input("Enter player number who starts."))
+	for i in range(0,6-start):
+		temp_dict.update({(start+i):data[start+i]})
+		#temp_dict[start+i]=data[start+i]
+	#print(temp_dict)
+	for i in range(1,start):
+		temp_dict.update({i:data[i]})
+		#temp_dict[i]=data[i]
+	#print(temp_dict)
 	while(len(temp_dict)>1):
 		print('Input the betting money after each displayed name\n ')
 		temp_dict, stake =chance(temp_dict,stake)
@@ -72,3 +82,4 @@ while(gameStatus==1):
 	print(balance)
 	print('data')
 	print(data)'''
+
